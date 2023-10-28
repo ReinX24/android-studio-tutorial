e9c9888c60da8b77e2344ac822478fd2a257f961
Rein Solis <reinX244@gmail.com>
2023-10-28 15:51:09 +0800

    Added BirthdayCard android project.

---
+++ BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.birthdaycard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycard.ui.theme.BirthdayCardTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    Greeting("Android")
+//                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                    GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+
+@Composable // a composable function must have a modifier parameter
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center, // aligns entire component
+        modifier = modifier
+    ) { // passing in modifier to our column elements
+        Text(
+            text = message,
+            fontSize = 100.sp, // sp means scalable pixels
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End) // aligns text within the container
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty) // importing image from res/drawable/..
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+
+}
+
+@Preview(
+    // Passing multiple parameters to our annotation
+    // These are not in reflected in our app, this only serves as a preview
+    showBackground = true,
+    showSystemUi = true, // shows preview when we run it on an android device
+    name = "My Preview"
+)
+@Composable
+fun BirthdayCardPreview() { // in pascal case and must describe component as a noun
+    BirthdayCardTheme {
+//        Greeting("Rein")
+//        GreetingText(message = "Happy Birthday Rein!", from = "From Mercy")
+        GreetingImage(message = "Happy Birthday Rein!", from = "From Mercy")
+    }
+
+}
